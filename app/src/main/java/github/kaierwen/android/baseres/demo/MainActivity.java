50252988d3aaf04ffb02d8bbb775fdcdb5ecec0a
kaierwen <mrkaierwen@gmail.com>
2019-07-16 17:35:38 +0800

    版本设为0.0.1

--- app/src/main/java/github/kaierwen/android/baseres/demo/MainActivity.java
+++ app/src/main/java/github/kaierwen/android/baseres/demo/MainActivity.java
@@ -1,8 +1,11 @@
 package github.kaierwen.android.baseres.demo;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import github.kaierwen.android.baseres.Version;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("TAG_BASE_RES", Version.getVersion());
     }
 }
---
+++ library/src/main/java/github/kaierwen/android/baseres/Version.java
@@ -0,0 +1,12 @@
+package github.kaierwen.android.baseres;
+
+/**
+ * @author kaierwen
+ * @since 2019/7/16
+ */
+public class Version {
+
+    public static String getVersion() {
+        return BuildConfig.VERSION_NAME;
+    }
+}
